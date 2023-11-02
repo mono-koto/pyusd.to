ab3d93fc6ce86c142509a666b6f6920d60c10234
Brent Fitzgerald <brent@mono-koto.com>
2023-11-02 11:21:01 -0700

    add wouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { PayCard } from "@/components/paycard";
+import { Route, Router } from "wouter";
+
+import { useHashLocation } from "./hooks/useHashLocation";
+import About from "./pages/About";
+import Pay from "./pages/Pay";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <PayCard />
+        <Router hook={useHashLocation}>
+          <Route path="" component={Home} />
+          <Route path="about" component={About} />
+          <Route path=":address" component={Pay} />
+        </Router>
       </div>
     </>
   );
